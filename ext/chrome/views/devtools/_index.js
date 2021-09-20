c7695a10983c61bb042beb9abe9bfb4ed889cdd8
Vitali Malinouski <vitmalina@gmail.com>
2021-09-20 12:13:04 -0700

    Dev tools panel

---
+++ ext/chrome/views/devtools/_index.js
@@ -0,0 +1,4 @@
+// This file runs in the devtools API and creates a panel
+chrome.devtools.panels.create("Bela Automation", null, 'views/devtools/panel.html', function (panel) {
+    // when panel is created
+})
---
+++ ext/edge/readme.md
@@ -0,0 +1,3 @@
+# Installation
+
+Miscrosoft Edge is based on Chromium project, as such it supports all of the extensions from Chrome.
